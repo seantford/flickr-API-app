129a8b9becba893d062bbbcc09bde87937beb6ac
Sean Ford <seanthomasford@gmail.com>
2015-10-09 18:36:23 -0400

    http>https in js and html

--- js/flickr.js
+++ js/flickr.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     $(this).addClass("selected");
 
     // the AJAX part
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var animal = $(this).text();
     var flickrOptions = {
       tags: animal,
